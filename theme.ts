030840a7e1aa14290514e7d1b3e71e4c52b70c0a
gedev <geniilsonfernandes@gmail.com>
2025-01-29 18:31:06 -0300

    Add custom color palette and set primary color in theme

--- theme.ts
+++ theme.ts
@@ -4,4 +4,19 @@ import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
+  colors: {
+    estimou: [
+      '#f0faf1',
+      '#dff2e1',
+      '#b9e5be',
+      '#91d898',
+      '#70cd78',
+      '#5cc664',
+      '#50c359',
+      '#41ab49',
+      '#379840',
+      '#2a8433',
+    ],
+  },
+  primaryColor: 'estimou',
 });
