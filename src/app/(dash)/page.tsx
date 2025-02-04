34719663bb9f885898b4fff255f5df79b26a256a
gedev <geniilsonfernandes@gmail.com>
2025-02-04 09:16:24 -0300

    feat: add Card component and integrate Recharts for analytics visualization; update styles and layout in Analytics component

--- src/app/(dash)/page.tsx
+++ src/app/(dash)/page.tsx
@@ -1,62 +1,11 @@
+import { Analytics } from '@/components/AnalyticsCard/Analytics'
 import { BudgetList } from '@/components/BudgetList/BudgetList'
-import {
-  IconBell,
-  IconScriptPlus,
-  IconSettings,
-  IconTagPlus,
-  IconUser,
-  IconUserPlus,
-} from '@tabler/icons-react'
-import { format } from 'date-fns'
+import { Notifications } from '@/components/Notifications/Notifications'
+import { Button } from '@/components/ui/button'
+import { UserInfo } from '@/components/UserInfo/UserInfo'
+import { IconScriptPlus, IconTagPlus, IconUserPlus } from '@tabler/icons-react'
 
 import { forwardRef } from 'react'
-
-const UserInfo = () => {
-  return (
-    <div aria-label="user info group" className="flex justify-between">
-      <div>
-        <h2 className="text-sm text-gray-500">Hello. Welcome back,</h2>
-        <h3 className="text-lg font-semibold text-gray-900">John Doe</h3>
-      </div>
-      <div className="h-10 w-10 rounded-full bg-gray-200">
-        <IconUser strokeWidth={1} />
-      </div>
-    </div>
-  )
-}
-
-const NotificationsCard: React.FC = () => {
-  return (
-    <div className="relative">
-      <div className="relative z-10 flex flex-1 items-center justify-between rounded-md bg-gray-900 p-4 py-6 shadow-md">
-        <div>
-          <span className="text-xs text-gray-400">{format(new Date(), 'PP')}</span>
-          <h1 className="text-md font-semibold text-white">Suas Notificações irão aparecer aqui</h1>
-        </div>
-        <button className="flex h-10 w-10 items-center justify-center rounded-lg bg-brand-200 text-gray-700 active:bg-brand-300">
-          <IconBell strokeWidth={1} />
-        </button>
-      </div>
-      <div className="relative mx-2 -mt-[18px] h-6 rounded-md bg-blue-200"></div>
-    </div>
-  )
-}
-
-const AnalyticsCard: React.FC = () => {
-  return (
-    <div className="flex items-center justify-between">
-      <div>
-        <h1 className="text-lg font-semibold text-gray-900">Seus orcamentos este mes</h1>
-        <span className="text-sm text-gray-500">
-          234 <span className="text-gray-500">orcamentos</span> foram criados
-        </span>
-      </div>
-      <button className="flex h-10 w-10 items-center justify-center rounded-lg border text-gray-700">
-        <IconSettings strokeWidth={1} />
-      </button>
-    </div>
-  )
-}
 
 interface ButtonWithIconProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string
@@ -85,11 +34,20 @@ export default function HomePage() {
     <div className="flex flex-col md:flex-row">
       <div className="flex-1 space-y-4 p-8">
         <UserInfo />
-        <NotificationsCard />
+        <Notifications />
         <div className="flex flex-col items-center justify-between gap-4 lg:flex-row">
-          <ButtonWithIcon label="Orcamento" icon={<IconScriptPlus strokeWidth={1} size={18} />} />
-          <ButtonWithIcon label="Cliente" icon={<IconUserPlus strokeWidth={1} size={18} />} />
-          <ButtonWithIcon label="Servico" icon={<IconTagPlus strokeWidth={1} size={18} />} />
+          <Button className="w-full text-left" variant={'secondary'}>
+            <IconScriptPlus strokeWidth={1} size={18} />
+            Budget
+          </Button>
+          <Button className="w-full text-left" variant={'secondary'}>
+            <IconUserPlus strokeWidth={1} size={18} />
+            Client
+          </Button>
+          <Button className="w-full text-left" variant={'secondary'}>
+            <IconTagPlus strokeWidth={1} size={18} />
+            Service
+          </Button>
         </div>
         <BudgetList
           budgets={Array.from({ length: 5 }).map((_, index) => ({
@@ -100,8 +58,8 @@ export default function HomePage() {
           }))}
         />
       </div>
-      <div className="min-h-screen flex-1 bg-gray-100 p-8">
-        <AnalyticsCard />
+      <div className="min-h-screen flex-1 bg-gray-100 py-8">
+        <Analytics />
       </div>
     </div>
   )
--- src/components/AnalyticsCard/Analytics.tsx
+++ src/components/AnalyticsCard/Analytics.tsx
@@ -1,17 +1,97 @@
-import { Settings2 } from 'lucide-react'
+'use client'
+
+import { Settings2, TrendingUp } from 'lucide-react'
+import { Bar, BarChart, CartesianGrid, XAxis } from 'recharts'
+
+import { Card } from '@/components/ui/card'
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart'
+import { Button } from '../ui/button'
+const chartData = [
+  { month: 'January', desktop: 186, mobile: 80 },
+  { month: 'February', desktop: 305, mobile: 200 },
+  { month: 'March', desktop: 237, mobile: 120 },
+  { month: 'April', desktop: 73, mobile: 190 },
+  { month: 'May', desktop: 209, mobile: 130 },
+  { month: 'June', desktop: 214, mobile: 140 },
+  { month: 'July', desktop: 214, mobile: 140 },
+  { month: 'August', desktop: 214, mobile: 140 },
+  { month: 'September', desktop: 214, mobile: 140 },
+  { month: 'October', desktop: 214, mobile: 140 },
+  { month: 'November', desktop: 214, mobile: 140 },
+  { month: 'December', desktop: 214, mobile: 140 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: 'Desktop',
+    color: '#363636',
+  },
+  mobile: {
+    label: 'Mobile',
+    color: '#949494',
+  },
+} satisfies ChartConfig
 
 export const Analytics: React.FC = () => {
   return (
-    <div className="flex items-center justify-between">
-      <div>
-        <h1 className="text-lg font-semibold text-gray-900">Seus orcamentos este mes</h1>
-        <span className="text-sm text-gray-500">
-          234 <span className="text-gray-500">orcamentos</span> foram criados
-        </span>
+    <div className="px-8">
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="text-lg font-semibold text-gray-900">Seus orcamentos este mes</h1>
+          <span className="text-sm text-gray-500">
+            234 <span className="text-gray-500">orcamentos</span> foram criados
+          </span>
+        </div>
+        <Button variant="ghost">
+          <Settings2 strokeWidth={1} />
+        </Button>
       </div>
-      <button className="flex h-10 w-10 items-center justify-center rounded-lg border text-gray-700">
-        <Settings2 strokeWidth={1} />
-      </button>
+      <div className="my-6 flex items-center gap-2">
+        {['6 months', '2022', '2023', '2024', '2025'].map((year) => (
+          <Button
+            size={'sm'}
+            key={year}
+            variant={year === '2025' ? 'default' : 'outline'}
+            className="cursor-pointer rounded-full text-sm"
+          >
+            {year}
+          </Button>
+        ))}
+      </div>
+
+      <div>
+        <ChartContainer config={chartConfig}>
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+            <ChartLegend content={<ChartLegendContent />} />
+            <Bar dataKey="desktop" stackId="a" fill="var(--color-desktop)" radius={[0, 0, 4, 4]} />
+            <Bar dataKey="mobile" stackId="a" fill="var(--color-mobile)" radius={[4, 4, 0, 0]} />
+          </BarChart>
+        </ChartContainer>
+        <Card className="my-8 space-y-2 p-4">
+          <div className="flex gap-2 text-sm font-medium leading-none">
+            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+          </div>
+          <div className="text-xs leading-none text-muted-foreground">
+            Showing total visitors for the last 6 months
+          </div>
+        </Card>
+      </div>
     </div>
   )
 }
