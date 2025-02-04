25cc74f2090fafc4938217a2813fcff4880031c5
gedev <geniilsonfernandes@gmail.com>
2025-02-03 21:15:06 -0300

    feat: add BudgetItem and BudgetList components for budget management

---
+++ src/components/BudgetItem/BudgetItem.tsx
@@ -0,0 +1,25 @@
+import { IconScript } from '@tabler/icons-react'
+import { Badge } from '../ui/badge'
+
+export interface BudgetItemProps {
+  title: string
+  date: string
+  status: string
+  amount: string
+}
+
+export const BudgetItem: React.FC<BudgetItemProps> = ({ title, date, status, amount }) => {
+  return (
+    <div className="inline-flex w-full cursor-pointer items-center gap-2 rounded-lg border border-gray-100 bg-gray-50 p-2 text-gray-700 hover:bg-gray-100">
+      <div className="rounded-md bg-indigo-300 p-2">
+        <IconScript strokeWidth={1} size={24} />
+      </div>
+      <div className="flex flex-1 flex-col items-start justify-start">
+        <h1 className="text-sm font-semibold">{title}</h1>
+        <span className="text-xs text-gray-500">{date}</span>
+      </div>
+      <Badge className="mx-4">{status}</Badge>
+      <span className="ml-auto text-xs font-semibold">{amount}</span>
+    </div>
+  )
+}
