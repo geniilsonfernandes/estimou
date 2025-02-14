be6a9e6db78c5a9e3b24fadcd2b25dd04daf64a5
gedev <geniilsonfernandes@gmail.com>
2025-02-14 08:33:30 -0300

    feat: Refactor Input component to use class-variance-authority for styling

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,20 +2,15 @@ import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 import { IconEye, IconEyeOff } from '@tabler/icons-react'
+import { cva } from 'class-variance-authority'
+
+const inputVariants = cva(
+  'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus:bg-brand-100/70 hover:bg-brand-100/70 focus:border-brand-300 hover:border-brand-300 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm transition-all aria-[invalid=true]:bg-red-100/70 aria-[invalid=true]:border-red-300'
+)
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
   ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus:border-brand-300 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
+    return <input type={type} className={cn(inputVariants(), className)} ref={ref} {...props} />
   }
 )
 
@@ -34,16 +29,13 @@ const InputPassword = React.forwardRef<HTMLInputElement, React.ComponentProps<'i
         <input
           ref={ref}
           type={isVisible ? 'text' : 'password'}
-          className={cn(
-            'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus:border-brand-300 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-            className
-          )}
+          className={cn(inputVariants(), className)}
           {...props}
         />
         <button
           type="button"
           onClick={toggleVisibility}
-          className="absolute right-3 top-1/2 -translate-y-1/2 transform text-gray-500"
+          className="absolute right-3 top-1/2 -translate-y-1/2 transform text-brand-500"
         >
           {isVisible ? <IconEye size={18} /> : <IconEyeOff size={18} />}
         </button>
