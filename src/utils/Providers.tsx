dcfc19e3bc95dc525dce9ac50dd7b17dfee7ecf4
gedev <geniilsonfernandes@gmail.com>
2025-02-15 23:36:56 -0300

    refactor: Update Providers component to use fragment for children rendering

--- src/utils/Providers.tsx
+++ src/utils/Providers.tsx
@@ -1,9 +1,12 @@
-"use client"
-
+'use client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
 export const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <>{children}</>
+    </QueryClientProvider>
+  )
 }
