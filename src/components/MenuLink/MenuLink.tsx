bcd8ac5d3c8bbe833226fe94cc5e22625df14ca9
gedev <geniilsonfernandes@gmail.com>
2025-02-21 15:27:53 -0300

    feat: enhance MenuLink component styles for improved interactivity and responsiveness

--- src/components/MenuLink/MenuLink.tsx
+++ src/components/MenuLink/MenuLink.tsx
@@ -6,34 +6,37 @@ import Link from 'next/link'
 import { cva, type VariantProps } from 'class-variance-authority'
 import { ClassValue } from 'clsx'
 
-const menuLinkVariants = cva('flex items-center gap-4 p-3  text-sm capitalize rounded-lg', {
-  variants: {
-    variant: {
-      primary: ' text-white',
-      secondary: 'bg-gray-200 text-gray-900',
-      outline: ' text-gray-900',
-      transparent: 'bg-transparent text-gray-600 hover:bg-white/50',
+const menuLinkVariants = cva(
+  'flex items-center gap-4 hover:gap-[14px] transition-all px-3 text-sm font-normal capitalize rounded-lg',
+  {
+    variants: {
+      variant: {
+        primary: ' text-white',
+        secondary: 'bg-gray-200 text-gray-900',
+        outline: ' text-gray-900',
+        transparent: 'bg-transparent text-gray-400 hover:bg-white/50',
+      },
+      hiddenLabel: {
+        true: 'justify-center lg:justify-start',
+        false: '',
+      },
+      size: {
+        sm: 'py-1 text-xs',
+        md: 'p-2 px-5 text-sm ',
+        lg: 'py-4 text-base',
+      },
+      active: {
+        true: 'text-brand-500  bg-brand/10 hover:bg-brand/15 ',
+        false: '',
+      },
     },
-    hiddenLabel: {
-      true: 'justify-center lg:justify-start',
-      false: '',
+    defaultVariants: {
+      variant: 'primary',
+      size: 'md',
+      hiddenLabel: true,
     },
-    size: {
-      sm: 'py-1 text-xs',
-      md: 'py-3 text-sm',
-      lg: 'py-4 text-base',
-    },
-    active: {
-      true: ' bg-gradient-to-r from-brand-400/15 to-brand-100/10 text-brand-500 [&_svg]:text-brand-600 hover:bg-white/10',
-      false: '',
-    },
-  },
-  defaultVariants: {
-    variant: 'primary',
-    size: 'md',
-    hiddenLabel: true,
-  },
-})
+  }
+)
 
 interface MenuLinkProps
   extends React.AnchorHTMLAttributes<HTMLAnchorElement>,
