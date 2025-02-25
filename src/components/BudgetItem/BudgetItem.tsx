7b19dc81861014ce9a62ff81e1fe41ace1d8bf48
gedev <geniilsonfernandes@gmail.com>
2025-02-24 22:31:05 -0300

    feat: enhance BudgetItem component with hover effects and additional props

--- src/components/BudgetItem/BudgetItem.tsx
+++ src/components/BudgetItem/BudgetItem.tsx
@@ -1,37 +1,150 @@
-import { Scroll } from 'lucide-react'
-import { Badge } from '../ui/badge'
+'use client'
 
-export interface BudgetItemProps {
-  title: string
-  date: string
-  status: string
-  amount: string
+import { cn } from '@/lib/utils'
+import { format } from 'date-fns'
+import {
+  Building,
+  Calendar,
+  CheckCheck,
+  CheckCircle,
+  DollarSign,
+  Edit,
+  Eye,
+  Loader,
+  Scroll,
+} from 'lucide-react'
+import { AnimatePresence, motion } from 'motion/react'
+import { useRef, useState } from 'react'
+
+export type Company = {
+  name: string
+  industry: string
+  location: string
 }
 
-export const BudgetItem: React.FC<BudgetItemProps> = ({ title, date, status, amount }) => {
-  return (
-    <div className="inline-flex w-full cursor-pointer flex-col overflow-hidden rounded-lg border border-gray-200 bg-gray-50 text-gray-700 transition-all duration-500 ease-out hover:translate-y-[2px] hover:bg-gray-100">
-      <div className="flex gap-2 p-2">
-        <div className="flex h-8 w-8 items-center justify-center rounded-md border border-gray-200 bg-secondary text-gray-500">
-          <Scroll size={16} />
-        </div>
+export type BudgetItemProps = {
+  title: string
+  date: string
+  status: 'Pending' | 'Completed' | 'In Progress'
+  amount: number
+  company?: Company
+  view?: boolean
+}
 
-        <div className="flex flex-1 flex-col items-start justify-start">
-          <h1 className="text-xs font-semibold">{title}</h1>
-          <span className="text-xs text-gray-500">{date}</span>
-        </div>
-        <div className="flex gap-4">
-          <div className="flex flex-1 flex-col items-start justify-start">
-            <h1 className="text-xs font-semibold">Valor</h1>
-            <span className="text-xs text-gray-500">{amount}</span>
+export const BudgetItem: React.FC<BudgetItemProps> = ({
+  title,
+  date,
+  status,
+  amount,
+  company,
+  view,
+}) => {
+  const wrapper = useRef<HTMLDivElement>(null)
+  const [isHovered, setIsHovered] = useState(false)
+
+  const height = 48
+  const heightHovered = 90
+
+  const statusColor = {
+    Pending: 'bg-yellow-100 text-yellow-600 border-yellow-300',
+    Completed: 'bg-green-100 text-green-600 border-green-300',
+    'In Progress': 'bg-blue-100 text-blue-600 border-blue-300',
+  }
+  const statusIcon = {
+    Pending: <Loader size={12} />,
+    Completed: <CheckCircle size={12} />,
+    'In Progress': <Loader size={12} />,
+  }
+  return (
+    <AnimatePresence>
+      <motion.div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        className="flex w-full cursor-pointer flex-col justify-between rounded-xl border border-gray-100 bg-gray-50 p-2"
+        animate={{ height: isHovered ? heightHovered : height }}
+        initial={false}
+        exit={{ height: height }}
+        transition={{
+          type: 'spring',
+          stiffness: 300,
+          damping: 30,
+        }}
+        layout
+        ref={wrapper}
+      >
+        <div className="flex items-center justify-between gap-2">
+          <div className="flex h-8 w-8 items-center justify-center rounded-lg border border-brand-300/40 bg-brand-200/40 text-brand-600">
+            <Scroll size={16} strokeWidth={1.4} />
           </div>
 
-          <Badge className="" variant="outline">
-            <span className="mr-2 h-2 w-2 rounded-full bg-red-600" />
-            {status}
-          </Badge>
+          <div className="flex flex-1 flex-col items-start justify-start">
+            <h1 className="text-xs font-semibold">{title}</h1>
+            <div className="flex gap-1 text-xs text-gray-500">
+              <Building size={14} strokeWidth={1} />
+              {company?.name}
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <div
+              className={cn(
+                'flex items-center gap-2 rounded-md border border-green-300 bg-green-100 p-1 px-2 text-xs text-green-600 transition-all duration-500 ease-in-out',
+                statusColor[status]
+              )}
+            >
+              {statusIcon[status]}
+              {status}
+            </div>
+            <span className="flex items-center gap-2 rounded-md border bg-white p-1 px-2 text-xs text-gray-500 transition-all duration-500 ease-in-out">
+              <DollarSign size={16} />
+              {amount.toLocaleString('pt-BR', { currency: 'BRL' })}
+            </span>
+          </div>
         </div>
-      </div>
-    </div>
+        {isHovered && (
+          <motion.div
+            className="mt-auto flex items-center justify-between rounded-md text-xs text-gray-500"
+            initial={{ opacity: 0, y: -8, filter: 'blur(4px)' }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              filter: 'blur(0px)',
+              transition: { duration: 0.2, delay: 0.1 },
+            }}
+            exit={{ opacity: 0, y: -8 }}
+          >
+            <span
+              className={cn(
+                'rounded-mdbg-white flex items-center gap-2 p-1 px-2 text-xs text-green-500 transition-all duration-500 ease-in-out',
+                {
+                  'text-red-600': !view,
+                }
+              )}
+            >
+              {view ? (
+                <CheckCheck size={16} strokeWidth={1} />
+              ) : (
+                <CheckCheck size={16} strokeWidth={1} />
+              )}
+
+              {view ? 'Visualizado' : 'Não visualizado'}
+            </span>
+            <div className="flex gap-1 text-xs text-gray-500">
+              <Calendar size={14} strokeWidth={1} />
+              {format(new Date(date), 'dd/MM/yyyy')}
+            </div>
+            <div className="flex gap-2">
+              <button className="flex items-center gap-2 rounded-md border bg-white p-1 px-2 text-xs transition-all duration-500 ease-in-out hover:bg-gray-100">
+                <Eye size={16} strokeWidth={1} />
+                Preview
+              </button>
+              <button className="flex items-center gap-2 rounded-md border bg-white p-1 px-2 text-xs transition-all duration-500 ease-in-out hover:bg-gray-100">
+                <Edit size={16} strokeWidth={1} />
+                Editar
+              </button>
+            </div>
+          </motion.div>
+        )}
+      </motion.div>
+    </AnimatePresence>
   )
 }
--- src/components/BudgetList/BudgetList.tsx
+++ src/components/BudgetList/BudgetList.tsx
@@ -6,31 +6,12 @@ interface BudgetListProps {
 }
 
 export const BudgetList: React.FC<BudgetListProps> = ({ budgets }) => {
-  const statusOptions = [
-    { label: 'Pending', value: 'Pending' },
-    { label: 'Approved', value: 'Approved' },
-    { label: 'Rejected', value: 'Rejected' },
-  ]
-
   return (
     <div className="card flex flex-col gap-2">
       <div className="flex items-center justify-between">
-        <span className="text-xs text-gray-500">Orcamentos recentes</span>
-        <div className="flex items-center gap-2">
-          <span className="text-xs text-gray-500">Filtrar por:</span>
-          <Button variant="default" size="xs">
-            Todos
-          </Button>
-          <Button size="xs" variant="outline">
-            Pendentes
-          </Button>
-          <Button size="xs" variant="outline">
-            Aprovados
-          </Button>
-        </div>
+        <span className="text-xs text-gray-500">Recentes</span>
       </div>
       <div className="space-y-2">
-        <BudgetItem title="Orcamento 1" date="22/01/2023" status="Pendente" amount="R$ 2.000,00" />
         {budgets.map((budget) => (
           <BudgetItem key={budget.title} {...budget} />
         ))}
